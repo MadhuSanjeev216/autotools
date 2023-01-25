a7c39ae7c7a1d6a17db004f6f1280e886c8ffc78
Madhu Sanjeev <madhusanjeev216@gmail.com>
2023-01-25 17:50:03 -0500

    added new file nav bar

---
+++ cypress/e2e/home_navbar.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+describe('Navigation through navbar in header', () => {
+
+    it('verify nav bar',()=>{
+        cy.login('testuserone@kubecloudsinc.com','Testing1');
+
+    })
+    
+})
