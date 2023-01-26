0c50efc72b78bd1a97c7402cd2807d8d1b796d0f
Madhu Sanjeev <madhusanjeev216@gmail.com>
2023-01-26 13:20:25 -0500

    All enteries of Region Details page are verified

--- cypress/e2e/regionDetails.cy.js
+++ cypress/e2e/regionDetails.cy.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 describe('verify region page', () =>{
    it('open base url', () =>{
        cy.login('testuserone@kubecloudsinc.com','Testing1');
@@ -10,6 +12,14 @@ describe('verify region page', () =>{
         cy.task('log','verify all region entries');
         cy.get('h1').should('be.visible').should('have.text','Regions');
         cy.get('th[class="number"]').should('have.text','Id');
-        // cy.get('th[class=\'number\']/following-sibling::*')
+        cy.get('th[class="number"]+th').should('have.text','Name');
+        cy.contains('td[class="number"]','1');
+        cy.contains('a','Europe');
+        cy.get('td a').first();
+        cy.get('td[class="number"]').nextAll();
+        cy.get('td a').eq(1); 
+        cy.get('td a').eq(2);
+        cy.get('td a').last();
+    
     })
 })
