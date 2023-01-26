cb12cc6855ba64def5746cb7607edbb24ec7b7b3
Madhu Sanjeev <madhusanjeev216@gmail.com>
2023-01-26 13:41:34 -0500

    testing git commit msg

--- cypress/e2e/regionDetails.cy.js
+++ cypress/e2e/regionDetails.cy.js
@@ -20,6 +20,6 @@ describe('verify region page', () =>{
         cy.get('td a').eq(1); 
         cy.get('td a').eq(2);
         cy.get('td a').last();
-    
+    //hi to check git commit msg
     })
 })
