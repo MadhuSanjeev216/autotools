729014d7b5a6edee90c04ee4c7e759fdbdb19263
Madhu Sanjeev <madhusanjeev216@gmail.com>
2023-01-25 20:43:20 -0500

    nav bar tests are completed

--- cypress/e2e/home_navbar.cy.js
+++ cypress/e2e/home_navbar.cy.js
@@ -4,6 +4,10 @@ describe('Navigation through navbar in header', () => {
 
     it('verify nav bar',()=>{
         cy.login('testuserone@kubecloudsinc.com','Testing1');
+        cy.get('a[class="brand"]').click();
+        cy.get('a[class="dropdown-toggle"]').click();
+        cy.contains('a','Employee Details');
+        cy.contains('a','Region Details');
 
     })
     
