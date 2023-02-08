a0fbbf2df48d1adaf76d6318844458b45a6c7950
Madhu Sanjeev <madhusanjeev216@gmail.com>
2023-02-08 18:36:51 -0500

    prettier

--- cypress/e2e/employeeDetails.cy.js
+++ cypress/e2e/employeeDetails.cy.js
@@ -1,41 +1,41 @@
-describe("verify the employee details page", ()=>{
-    it("opens the login page", ()=>{
-
-        cy.login('testuserone@kubecloudsinc.com','Testing1');
-        cy.pageValidator();
-        //cy.contains('a','Employee Details')
-
-    })
-
-    it("navigate to employee details page", ()=>{
-        cy.get('li a').eq(3).click();
-        cy.pageValidator();
+describe("verify the employee details page", () => {
+  it("opens the login page", () => {
+    cy.login("testuserone@kubecloudsinc.com", "Testing1");
+    cy.pageValidator();
+  });
+  it("navigate to employee details page", () => {
+    cy.get("li a").eq(3).click();
+    cy.pageValidator();
     //    cy.request("/").its('body')
     //    .then(html=>{
     //         const $employee1 = Cypress.$(html).css('th','number').text();
     //    })
-    })
-    it("Verify first name in both summary page and employee page",()=>{
-            for(let i=0;i<=106;i++){
-            let name    
-                cy.get('td.number+td').eq(i).then(($n)=>{
-                        name = $n.text()+" ";
-                    })
-                cy.get('td.number+td+td').eq(i).then(($n)=>{
-                        name += $n.text();
-                   })   
-                cy.get('td.number a').eq(i).click();
-                cy.get('h1').then(($verifyName)=>{
-                        expect(name).to.contain($verifyName.text());
-                            // if(name==$verifyName.text()) {
-                            //     cy.task('log','Valid');
-                            // }
-                            // else {
-                            //     cy.task('log','Not Valid');
-                            //     console.error('Not a Valid Name')
-                            // }
-                    })                                                                           
-                 cy.go('back');
-            }
-    })
-})
+  });
+  it("Verify first name in both summary page and employee page", () => {
+    for (let i = 0; i <= 106; i++) {
+      let name;
+      cy.get("td.number+td")
+        .eq(i)
+        .then(($n) => {
+          name = $n.text() + " ";
+        });
+      cy.get("td.number+td+td")
+        .eq(i)
+        .then(($n) => {
+          name += $n.text();
+        });
+      cy.get("td.number a").eq(i).click();
+      cy.get("h1").then(($verifyName) => {
+        expect(name).to.contain($verifyName.text());
+        // if(name==$verifyName.text()) {
+        //     cy.task('log','Valid');
+        // }
+        // else {
+        //     cy.task('log','Not Valid');
+        //     console.error('Not a Valid Name')
+        // }
+      });
+      cy.go("back");
+    }
+  });
+});
